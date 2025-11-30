9437d77d889c813e315fe11dd29edc84242f2d49
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2025-11-30 12:01:48 +0100

    Added interface for types

--- include/moonsugar/api.h
+++ include/moonsugar/api.h
@@ -13,6 +13,17 @@
 #define MSMALLOC __attribute__((malloc))
 #define MSUSERET __attribute__((warn_unused_result))
 
+#define ms_u8 unsigned char
+#define ms_i8 char
+#define ms_u16 unsigned short
+#define ms_i16 short
+#define ms_u32 unsigned
+#define ms_i32 int
+#define ms_u64 unsigned long long
+#define ms_i64 long long
+#define ms_f32 float
+#define ms_f64 double
+
 typedef enum {
   /**
    * The operation was successful.
@@ -36,6 +47,5 @@ typedef enum {
    */
   MS_RESULT_INVALID_ARGUMENT
 } ms_result;
-
 
 #endif // MS_API_H
