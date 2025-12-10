7053ac9c7adaa0e68edf87ec8a6a9378f4eba66b
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2025-12-11 00:35:04 +0100

    Fixed alignment for OS memory interface

--- src/memory/os.unix.c
+++ src/memory/os.unix.c
@@ -21,8 +21,12 @@ void* ms_reserve(size_t count) {
    return result;
 }
 
-void ms_release(void * const ptr, size_t count) {
-  count = ms_align_sz(count, ms_get_sys_info()->page_size);
+void ms_release(void * ptr, size_t count) {
+  uint64_t const page_size = ms_get_sys_info()->page_size;
+  uint8_t* const ptr_end = ms_align_ptr((uint8_t*)ptr + count, page_size);
+
+  ptr = ms_align_back_ptr(ptr, page_size);
+  count = ptr_end - (uint8_t*)ptr;
 
   munmap(ptr, count);
 }
@@ -60,8 +64,12 @@ bool ms_commit(void * ptr, size_t count) {
   return result != MAP_FAILED;
 }
 
-void ms_decommit(void * const ptr, size_t count) {
-  count = ms_align_sz(count, ms_get_sys_info()->page_size);
+void ms_decommit(void * ptr, size_t count) {
+  uint64_t const page_size = ms_get_sys_info()->page_size;
+  uint8_t* const ptr_end = ms_align_ptr((uint8_t*)ptr + count, page_size);
+
+  ptr = ms_align_back_ptr(ptr, page_size);
+  count = ptr_end - (uint8_t*)ptr;
 
 #ifdef HAS_REMAP
   // TODO: Implement this correctly - good luck
--- src/memory/os.win32.c
+++ src/memory/os.win32.c
@@ -3,16 +3,24 @@
 #include <moonsugar/memory.h>
 #include <moonsugar/sys.h>
 
-void* ms_reserve(const size_t count) {
+void* ms_reserve(size_t count) {
+  count = ms_align_sz(count, ms_get_sys_info()->page_size);
+
   return VirtualAlloc(
-      NULL,
-      count,
-      MEM_RESERVE,
-      PAGE_READWRITE
+    NULL,
+    count,
+    MEM_RESERVE,
+    PAGE_READWRITE
   );
 }
 
-void ms_release(void * const ptr, size_t const count) {
+void ms_release(void * ptr, size_t count) {
+  uint64_t const page_size = ms_get_sys_info()->page_size;
+  uint8_t* const ptr_end = ms_align_ptr((uint8_t*)ptr + count, page_size);
+
+  ptr = ms_align_back_ptr(ptr, page_size);
+  count = ptr_end - (uint8_t*)ptr;
+
   VirtualFree(ptr, (SIZE_T)count, MEM_RELEASE);
 }
 
@@ -24,17 +32,19 @@ bool ms_commit(void * ptr, size_t count) {
   count = ptr_end - (uint8_t*)ptr;
 
   return VirtualAlloc(
-      ptr,
-      count,
-      MEM_COMMIT,
-      PAGE_READWRITE
+    ptr,
+    count,
+    MEM_COMMIT,
+    PAGE_READWRITE
   ) != NULL;
 }
 
-void ms_decommit(void * const ptr, const size_t count) {
-  VirtualFree(
-      ptr,
-      count,
-      MEM_DECOMMIT
-  );
+void ms_decommit(void * ptr, size_t count) {
+  uint64_t const page_size = ms_get_sys_info()->page_size;
+  uint8_t* const ptr_end = ms_align_ptr((uint8_t*)ptr + count, page_size);
+
+  ptr = ms_align_back_ptr(ptr, page_size);
+  count = ptr_end - (uint8_t*)ptr;
+
+  VirtualFree(ptr, count, MEM_DECOMMIT);
 }
