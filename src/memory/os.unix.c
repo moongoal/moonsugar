575efe2ff5a73e29007c2e068e16823cd4a4e935
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2025-12-11 00:23:01 +0100

    Fixed cross-page alignment

--- src/memory/os.unix.c
+++ src/memory/os.unix.c
@@ -29,9 +29,10 @@ void ms_release(void * const ptr, size_t count) {
 
 bool ms_commit(void * ptr, size_t count) {
   uint64_t const page_size = ms_get_sys_info()->page_size;
+  uint8_t* const ptr_end = ms_align_ptr((uint8_t*)ptr + count, page_size);
 
   ptr = ms_align_back_ptr(ptr, page_size);
-  count = ms_align_sz(count, page_size);
+  count = ptr_end - (uint8_t*)ptr;
 
 #ifdef HAS_REMAP
   // TODO: Implement this correctly - good luck
--- src/memory/os.win32.c
+++ src/memory/os.win32.c
@@ -1,32 +1,40 @@
 #include <Windows.h>
+#include <moonsugar/util.h>
 #include <moonsugar/memory.h>
+#include <moonsugar/sys.h>
 
 void* ms_reserve(const size_t count) {
-    return VirtualAlloc(
-        NULL,
-        count,
-        MEM_RESERVE,
-        PAGE_READWRITE
-    );
+  return VirtualAlloc(
+      NULL,
+      count,
+      MEM_RESERVE,
+      PAGE_READWRITE
+  );
 }
 
 void ms_release(void * const ptr, size_t const count) {
-    VirtualFree(ptr, (SIZE_T)count, MEM_RELEASE);
+  VirtualFree(ptr, (SIZE_T)count, MEM_RELEASE);
 }
 
-bool ms_commit(void * const ptr, const size_t count) {
-    return VirtualAlloc(
-        ptr,
-        count,
-        MEM_COMMIT,
-        PAGE_READWRITE
-    ) != NULL;
+bool ms_commit(void * ptr, size_t count) {
+  uint64_t const page_size = ms_get_sys_info()->page_size;
+  uint8_t* const ptr_end = ms_align_ptr((uint8_t*)ptr + count, page_size);
+
+  ptr = ms_align_back_ptr(ptr, page_size);
+  count = ptr_end - (uint8_t*)ptr;
+
+  return VirtualAlloc(
+      ptr,
+      count,
+      MEM_COMMIT,
+      PAGE_READWRITE
+  ) != NULL;
 }
 
 void ms_decommit(void * const ptr, const size_t count) {
-    VirtualFree(
-        ptr,
-        count,
-        MEM_DECOMMIT
-    );
+  VirtualFree(
+      ptr,
+      count,
+      MEM_DECOMMIT
+  );
 }
